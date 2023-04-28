91badf6051811dc03e863ea6ee9cccb8c6b157e8
Sofiqul Islam <sofiquldev@gmail.com>
2023-04-28 12:07:04 +0600

    add github page deploy code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = '', basePath = ''
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    unoptimized: true,
+  },
 }
+
+
 
 module.exports = nextConfig
